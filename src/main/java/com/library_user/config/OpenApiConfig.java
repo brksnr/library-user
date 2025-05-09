81e2bc0de33d8b36c6e54f6a318cab8c27586575
brksnr <berksener04@gmail.com>
2025-05-09 15:14:21 +0300

    add swagger.

---
+++ src/main/java/com/library_user/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.library_user.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Management API")
+                        .description("API for managing library")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Berk Şener")
+                                .email("berksener04@gmail.com")
+                                .url("https://github.com/brksnr"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:3000")
+                                .description("Local server")
+                ));
+    }
+
+}
