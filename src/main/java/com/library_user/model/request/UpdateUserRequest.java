173d8d8e8926b93b6bf70f26a2e58b585b67bc4a
brksnr <berksener04@gmail.com>
2025-05-10 00:43:38 +0300

    add UpdateUserRequest dto for update user.

---
+++ src/main/java/com/library_user/model/request/UpdateUserRequest.java
@@ -0,0 +1,16 @@
+package com.library_user.model.request;
+
+import com.library_user.model.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+public record UpdateUserRequest(
+        @NotBlank String name,
+        @NotBlank @Email String email,
+        @NotBlank String password,
+        @NotBlank String contact,
+        @NotBlank Role role,
+        @NotBlank Integer borrowedBookCount
+) {
+}
