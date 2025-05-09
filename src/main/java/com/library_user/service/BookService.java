77ba67ef1d9bba10787dfd62719406da41116ab5
brksnr <berksener04@gmail.com>
2025-05-09 16:15:38 +0300

    add BookService

---
+++ src/main/java/com/library_user/service/BookService.java
@@ -0,0 +1,18 @@
+package com.library_user.service;
+
+import com.library_user.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+}
