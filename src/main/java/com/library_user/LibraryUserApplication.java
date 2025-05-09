0f58f9f15b4da46523ae153308a85875c11b1915
brksnr <berksener04@gmail.com>
2025-05-09 15:08:16 +0300

    first commit

---
+++ src/main/java/com/library_user/LibraryUserApplication.java
@@ -0,0 +1,13 @@
+package com.library_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library_user/LibraryUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
