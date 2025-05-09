df5d2eb5297757625ac7b32d9fba2a054cf7abf3
brksnr <berksener04@gmail.com>
2025-05-09 21:33:55 +0300

    Implement custom user details service for JWT authentication

---
+++ src/main/java/com/library_user/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.library_user.security;
+
+import com.library_user.exceptions.CustomException;
+import com.library_user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException("User not found with email: " + email, HttpStatus.NOT_FOUND));
+    }
+}
