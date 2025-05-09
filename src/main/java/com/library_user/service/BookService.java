7cd7c40c25f6ae2dc2c5fd87bbba325d49ae03cd
brksnr <berksener04@gmail.com>
2025-05-10 01:02:31 +0300

    Refactor BookService into interface and implementation for SOLID compliance

--- src/main/java/com/library_user/service/BookService.java
+++ src/main/java/com/library_user/service/BookService.java
@@ -1,109 +1,20 @@
 package com.library_user.service;
 
-import com.library_user.exceptions.CustomException;
 import com.library_user.model.dto.BookDto;
-import com.library_user.model.entity.Book;
-import com.library_user.model.helper.ErrorMessages;
-import com.library_user.model.mapper.BookMapper;
-import com.library_user.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 import java.util.UUID;
 
-@Service
-public class BookService {
-
-    private final BookRepository bookRepository;
-
-    @Autowired
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
-    private void checkBookExistsByIsbn(String isbn) {
-        if (bookRepository.findByIsbn(isbn).isPresent()) {
-            throw new CustomException(
-                    String.format(ErrorMessages.BOOK_ALREADY_EXISTS_ISBN, isbn),
-                    HttpStatus.CONFLICT
-            );
-        }
-    }
-
-    public BookDto addBook(BookDto bookDto) {
-        checkBookExistsByIsbn(bookDto.getIsbn());
-        Book book = BookMapper.toEntity(bookDto);
-        if (book.getId() == null) {
-            book.setId(UUID.randomUUID());
-        }
-        Book saved = bookRepository.save(book);
-        return BookMapper.toDto(saved);
-    }
-
-    public BookDto updateBook(UUID id, BookDto bookDto) {
-        Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new CustomException(ErrorMessages.BOOK_NOT_FOUND_ID + id, HttpStatus.NOT_FOUND));
-        BookMapper.updateEntity(book, bookDto);
-        Book updated = bookRepository.save(book);
-        return BookMapper.toDto(updated);
-    }
-
-    public void deleteBook(UUID id) {
-        if (!bookRepository.existsById(id)) {
-            throw new CustomException(ErrorMessages.BOOK_NOT_FOUND_ID + id, HttpStatus.NOT_FOUND);
-        }
-        bookRepository.deleteById(id);
-    }
-
-    public BookDto getBookById(UUID id) {
-        Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new CustomException(ErrorMessages.BOOK_NOT_FOUND_ID + id, HttpStatus.NOT_FOUND));
-        return BookMapper.toDto(book);
-    }
-
-    public BookDto getBookByIsbn(String isbn) {
-        Book book = bookRepository.findByIsbn(isbn)
-                .orElseThrow(() -> new CustomException(ErrorMessages.BOOK_NOT_FOUND_ISBN + isbn, HttpStatus.NOT_FOUND));
-        return BookMapper.toDto(book);
-    }
-
-    public BookDto updateBookAvailability(UUID id, boolean availability) {
-        Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new CustomException(
-                        String.format(ErrorMessages.BOOK_NOT_FOUND_ID, id),
-                        HttpStatus.NOT_FOUND
-                ));
-        book.setAvailability(availability);
-        Book updated = bookRepository.save(book);
-        return BookMapper.toDto(updated);
-    }
-
-    public List<BookDto> searchBooksByTitle(String title, Pageable pageable) {
-        return bookRepository.findByTitleContainingIgnoreCase(title, pageable)
-                .map(BookMapper::toDto)
-                .getContent();
-    }
-
-    public List<BookDto> searchBooksByAuthor(String author, Pageable pageable) {
-        return bookRepository.findByAuthorContainingIgnoreCase(author, pageable)
-                .map(BookMapper::toDto)
-                .getContent();
-    }
-
-    public List<BookDto> searchBooksByGenre(String genre, Pageable pageable) {
-        return bookRepository.findByGenreContainingIgnoreCase(genre, pageable)
-                .map(BookMapper::toDto)
-                .getContent();
-    }
-
-    public List<BookDto> searchBooksByAvailability(boolean availability, Pageable pageable) {
-        return bookRepository.findByAvailability(availability, pageable)
-                .map(BookMapper::toDto)
-                .getContent();
-    }
-
+public interface BookService {
+    BookDto addBook(BookDto bookDto);
+    BookDto updateBook(UUID id, BookDto bookDto);
+    void deleteBook(UUID id);
+    BookDto getBookById(UUID id);
+    BookDto getBookByIsbn(String isbn);
+    BookDto updateBookAvailability(UUID id, boolean availability);
+    List<BookDto> searchBooksByTitle(String title, Pageable pageable);
+    List<BookDto> searchBooksByAuthor(String author, Pageable pageable);
+    List<BookDto> searchBooksByGenre(String genre, Pageable pageable);
+    List<BookDto> searchBooksByAvailability(boolean availability, Pageable pageable);
 }
