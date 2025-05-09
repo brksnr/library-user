f17123fc6fefd5b210a3ac439e600ab968db6454
brksnr <berksener04@gmail.com>
2025-05-09 21:27:18 +0300

    add User Repository

---
+++ src/main/java/com/library_user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.library_user.repository;
+
+import com.library_user.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
