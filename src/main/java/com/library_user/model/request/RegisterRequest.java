59fcddda33c1f441a85720157bdae1ee0aa72b83
brksnr <berksener04@gmail.com>
2025-05-11 21:01:52 +0300

    Add @Builder for requests.

--- src/main/java/com/library_user/model/request/RegisterRequest.java
+++ src/main/java/com/library_user/model/request/RegisterRequest.java
@@ -3,13 +3,15 @@ package com.library_user.model.request;
 import com.library_user.model.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
-
+@Builder
 public record RegisterRequest(
         @NotBlank String name,
         @NotBlank @Email String email,
         @NotBlank String password,
         @NotBlank String contact,
-        @NotBlank Role role
+        @NotNull Role role
 ) {
 }
--- src/main/java/com/library_user/model/request/UpdateUserRequest.java
+++ src/main/java/com/library_user/model/request/UpdateUserRequest.java
@@ -3,8 +3,9 @@ package com.library_user.model.request;
 import com.library_user.model.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
-
+@Builder
 public record UpdateUserRequest(
         @NotBlank String name,
         @NotBlank @Email String email,
