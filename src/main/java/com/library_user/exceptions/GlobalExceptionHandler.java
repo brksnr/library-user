235d15a1c2b035536b015f7a4671b9b384804c71
brksnr <berksener04@gmail.com>
2025-05-12 00:09:10 +0300

    Add comments to GlobalExceptionHandler methods explaining their functionality

--- src/main/java/com/library_user/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/library_user/exceptions/GlobalExceptionHandler.java
@@ -18,12 +18,16 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
 
+    // Handles CustomException thrown by the application.
     @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handleException(CustomException apiException){
         log.error("Exception occured: " , apiException);
         return new ResponseEntity<>(new ExceptionResponse(apiException.getMessage(),apiException.getHttpStatus().value(), LocalDateTime.now()),apiException.getHttpStatus());
     }
 
+    /**
+      Handles AuthorizationDeniedException, which occurs when the user is not authorized to perform an operation
+      */
     @ExceptionHandler(AuthorizationDeniedException.class)
     public ResponseEntity<ExceptionResponse> handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
         log.warn("AuthorizationDeniedException occurred: {}" , ex.getMessage());
@@ -31,6 +35,9 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ExceptionResponse(errorMessage, HttpStatus.FORBIDDEN.value(), LocalDateTime.now()), HttpStatus.FORBIDDEN);
     }
 
+    /**
+     Handles MethodArgumentNotValidException, which occurs when input validation fails for request parameters
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         log.warn("MethodArgumentNotValidException occurred: {}", ex.getMessage());
@@ -45,13 +52,19 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(AuthenticationException.class) // Veya BadCredentialsException.class
+    /**
+     Handles AuthenticationException, which occurs during authentication failures (incorrect username/password)
+     * */
+    @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ExceptionResponse> handleAuthenticationException(AuthenticationException ex) {
         log.warn("AuthenticationException occurred: {}", ex.getMessage());
         String errorMessage = "invalid username or password.";
         return new ResponseEntity<>(new ExceptionResponse(errorMessage, HttpStatus.UNAUTHORIZED.value(), LocalDateTime.now()), HttpStatus.UNAUTHORIZED);
     }
 
+    /**
+     Handles any general exception and returns an internal server error response
+     */
     @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handleException(Exception exception){
         log.error("Exception occured: " , exception);
