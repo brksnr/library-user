73b80eb85e74a90caa00784a4796ce4d3f3b678f
brksnr <berksener04@gmail.com>
2025-05-11 20:53:30 +0300

    Added @Builder AuthRequest

--- src/main/java/com/library_user/model/request/AuthRequest.java
+++ src/main/java/com/library_user/model/request/AuthRequest.java
@@ -1,7 +1,9 @@
 package com.library_user.model.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record AuthRequest(
         @NotBlank String email,
         @NotBlank String password
