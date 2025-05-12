c7a271a1a436e8ae593f96446981ece667f2d8b3
brksnr <berksener04@gmail.com>
2025-05-12 14:51:15 +0300

    Enable scheduling for overdue book checking.

--- src/main/java/com/library_user/LibraryUserApplication.java
+++ src/main/java/com/library_user/LibraryUserApplication.java
@@ -2,8 +2,10 @@ package com.library_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibraryUserApplication {
 
 	public static void main(String[] args) {
