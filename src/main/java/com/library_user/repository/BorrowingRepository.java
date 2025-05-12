883bb315311c89d49f8983f02ea40dff3b3b865b
brksnr <berksener04@gmail.com>
2025-05-12 14:51:24 +0300

    Exclude returned books from overdue status updates.

--- src/main/java/com/library_user/repository/BorrowingRepository.java
+++ src/main/java/com/library_user/repository/BorrowingRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface BorrowingRepository extends JpaRepository<Borrowing, UUID> {
 
     Page<Borrowing> findByUserId(UUID userId, Pageable pageable);
     Page<Borrowing> findByReturnDateIsNullAndDueDateBefore(LocalDate date, Pageable pageable);
+    List<Borrowing> findAllByDueDateBeforeAndOverdueFalseAndReturnDateIsNull(LocalDate date);
 
 }
