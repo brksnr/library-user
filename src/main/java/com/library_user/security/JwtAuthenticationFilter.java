120943ac99f5a535e79ff6206875347e3d7c2e5f
brksnr <berksener04@gmail.com>
2025-05-13 12:07:45 +0300

    Removed Component annotation

--- src/main/java/com/library_user/security/JwtAuthenticationFilter.java
+++ src/main/java/com/library_user/security/JwtAuthenticationFilter.java
@@ -8,12 +8,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
